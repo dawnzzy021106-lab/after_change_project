20153b3d1f016e33952ad116763188114b393a21
Zhao Ziyi <dawnzzy021106@gmail.com>
2026-06-24 08:41:35 -0700

    Move random repair client implementation into project

--- project/src/client/client.cpp
+++ project/src/client/client.cpp
@@ -151,11 +151,16 @@ namespace ECProject
 
   RepairResp Client::nodes_repair(std::vector<unsigned int> failed_node_ids)
   {
+    return nodes_random_repair(failed_node_ids);
+  }
+
+  RepairResp Client::nodes_random_repair(std::vector<unsigned int> failed_node_ids)
+  {
     auto r = async_simple::coro::syncAwait(
-        rpc_coordinator_->call_for<&Coordinator::request_repair>(
+        rpc_coordinator_->call_for<&Coordinator::request_random_repair>(
             std::chrono::seconds{500}, failed_node_ids, -1));
     if (!r) {
-      std::cerr << "[RPC ERROR] request_repair(nodes) failed: "
+      std::cerr << "[RPC ERROR] request_random_repair(nodes) failed: "
                 << r.error().msg << std::endl;
       return RepairResp{};
     }
